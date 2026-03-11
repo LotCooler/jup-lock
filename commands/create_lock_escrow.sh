a230900b7bbd9ce5d0167d7a8994043f78142e92
LotCooler <don.knudsen@gmail.com>
2026-03-11 11:36:20 -0700

    - updated create_lock_escrow.sh to fix paths in script so it can be run from the local folder (no PATH changes needed) - modified test.csv to adhere to format needed for create_lock_escrow.sh

--- commands/create_lock_escrow.sh
+++ commands/create_lock_escrow.sh
@@ -1,11 +1,13 @@
 rpc_url=https://api.devnet.solana.com
-keypair_path=
-wallet_path=cli/test.csv
-token_mint=
+keypair_path=../keys/localnet/admin-bossj3JvwiNK7pvjr149DqdtJxf2gdygbcmEPTkb2F1.json
+wallet_path=./trash/test.csv
+token_mint=3WjUy2UyyjHFBSCSvj5gWFvdSc3cKwssxXuj4MsR8tWL
 vesting_start_time=1728980663
 cliff_time=1729067063
 frequency=604800
 number_of_period=4
 update_recipient_mode=0
 
-target/debug/cli --rpc-url $rpc_url --keypair-path $keypair_path initialize-lock-escrow-from-file --wallet-path $wallet_path --token-mint $token_mint --vesting-start-time $vesting_start_time --cliff-time $cliff_time --frequency $frequency --number-of-period $number_of_period --update-recipient-mode $update_recipient_mode
+../target/debug/cli --rpc-url $rpc_url --keypair-path $keypair_path initialize-lock-escrow-from-file --wallet-path $wallet_path --token-mint $token_mint --vesting-start-time $vesting_start_time --cliff-time $cliff_time --frequency $frequency --number-of-period $number_of_period --update-recipient-mode $update_recipient_mode --cancel-mode 0
+
+read -p "Press Enter to continue..."
